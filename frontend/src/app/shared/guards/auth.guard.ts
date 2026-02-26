3fb38016e77516c63b3db0304b504c5cabf15cf8
tonnyBryan <tonnybryan007@gmail.com>
2026-02-27 01:28:27 +0300

    improve navigation speed

--- frontend/src/app/shared/guards/auth.guard.ts
+++ frontend/src/app/shared/guards/auth.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-import { map, catchError, of } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -13,19 +12,34 @@ export class AuthGuard implements CanActivate {
         private router: Router
     ) {}
 
-    canActivate() {
+    canActivate(): boolean {
         if (!this.auth.isLoggedIn()) {
             this.router.navigate(['/signin']);
-            return of(false);
+            return false;
         }
 
-        return this.auth.verifyToken().pipe(
-            map(() => true),
-            catchError(() => {
-                this.auth.logout();
-                this.router.navigate(['/signin']);
-                return of(false);
-            })
-        );
+        if (this.auth.isTokenExpired()) {
+            this.auth.logout();
+            this.router.navigate(['/signin']);
+            return false;
+        }
+
+        return true;
     }
 }
+
+// canActivate() {
+//     if (!this.auth.isLoggedIn()) {
+//         this.router.navigate(['/signin']);
+//         return of(false);
+//     }
+//
+//     return this.auth.verifyToken().pipe(
+//         map(() => true),
+//         catchError(() => {
+//             this.auth.logout();
+//             this.router.navigate(['/signin']);
+//             return of(false);
+//         })
+//     );
+// }
